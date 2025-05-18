b1e3391ca3134bc8a473576d100aad85d2a953fe
Ursego <90226705+Ursego@users.noreply.github.com>
2025-05-18 17:15:41 -0400

    Add files via upload

--- 06 Data Types - Narrowing & Type Guards.ts
+++ 06 Data Types - Narrowing & Type Guards.ts
@@ -1,122 +1,73 @@
-//### Narrowing
-
 // Narrowing refers to the process of refining the type of a variable from a broader type to a more specific type.
 // Narrowing occurs when TypeScript can deduce a more specific type for a value based on the structure of the code.
 // This can be achieved using different techniques like typeof for primitive types, instanceof for objects, and type guard functions like Array.isArray.
 // These techniques help TypeScript to better understand the types at runtime, enabling more precise type checking and reducing the risk of runtime errors.
 
-// Narrowing with typeof for primitives:
+// @@@ Narrowing with 'typeof' for primitives
+
 function printId(id: number | string) {
-	if (typeof id === "string") {
+	if (typeof id === "string") { // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 		console.log(id.toUpperCase());
 	} else {
 		console.log(id);
 	}
 }
 
-// Narrowing with instanceof for Objects:
+// @@@ Narrowing with 'instanceof' for Objects
+
+// instanceof reports if the object is of a particular class or its descendant (directly or through inheritance chain).
+
 class Dog {
-  bark() { console.log("Woof! Woof!"); }
+  bark() { console.log("Woof!"); }
 }
 class Cat {
-  meow() { console.log("Meow! Meow!"); }
+  meow() { console.log("Meow!"); }
 }
 function makeNoise(animal: Dog | Cat) {
-  if (animal instanceof Dog) {
+  if (animal instanceof Dog) { // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     animal.bark();
   } else {
     animal.meow();
   }
 }
-// Stightly re-written version of makeNoise - using a pointer to a function (a function var):
-function makeNoise(animal: Dog | Cat) {
-  const makeSound = (animal instanceof Dog) ? animal.bark.bind(animal) : animal.meow.bind(animal);
-  makeSound();
+
+// Note that instanceof only works with classes, but not with interfaces.
+// The instanceof operator is a JavaScript runtime operator that checks if an object is an instance of a particular class or constructor function.
+// instanceof cannot be used with interfaces since they are compile-time constructs that don't exist at runtime - they are completely erased during compilation to JavaScript.
+
+// @@@ Narrowing with 'in' for interfaces and plain objects
+
+// Before accessing a memeber property (function or field) of the checked object, simply make sure that the property exists in it by using the 'in' operator:
+
+interface Dog {
+  bark(): void;
 }
-// instanceof reports of one object is inherited from another (directly or through inheritance chain)
-murka instanceof cat // true
-murka instanceof mammal // true
-murka instanceof animal // true
 
-// @@@ Type guard function with "is"
-
-// Type guards in TypeScript are functions or constructs that allow you to narrow down the type of a variable within a conditional block.
-// One powerful feature of TypeScript is the ability to create custom type guards using the is keyword.
-
-// A type guard function is a function that returns a type predicate.
-// A type predicate takes the form "paramName is Type", where paramName is the name of the parameter, and Type is the type you're checking for.
-// This allows TypeScript to infer the type of a variable within the scope of a conditional block.
-
-// They return boolean even though the return type is not marked with the "boolean" keyword. 
-// But they also inform TypeScript about the type of a variable if the condition is met (and true is returned).
-
-// The next examples use the Dog and Cat classes created earlier.
-function isDog(animal: Dog | Cat): animal is Dog {
-  return (animal as Dog).bark !== undefined;
+interface Cat {
+  meow(): void;
 }
-function isCat(animal: Dog | Cat): animal is Cat {
-  return (animal as Cat).meow !== undefined;
-}
-function makeNoise(animal: Dog | Cat) {
-  if (isDog(animal)) {
-    animal.bark(); // if isDog would return "boolean" rather than "animal is Dog", this line would produce a compile error
-  } else if (isCat(animal)) {
+
+function makeSound(animal: Dog | Cat) {
+  if ("bark" in animal) { // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    animal.bark();
+  } else {
     animal.meow();
   }
 }
-// Using "instanceof" can be simpler and more straightforward for many common cases, and would work in our example:
-function isDog(animal: Dog | Cat): animal is Dog {
-  return animal instanceof Dog;
-}
-function isCat(animal: Dog | Cat): animal is Cat {
-  return animal instanceof Cat;
-}
-// The reason for sometimes using the (animal as Dog).bark !== undefined pattern is to demonstrate a more general approach that can handle cases
-//    where instanceof might not work as expected, such as with interfaces or plain objects.
 
-// @@@ 'this'-based type guard function
+// Note that the property is passed as a string. If it's a function, the function name is passed without ().
 
-// You can use 'this is Type' in the return position for methods in classes and interfaces.
-// When mixed with a type narrowing (e.g. if statements) the type of the target object would be narrowed to the specified Type:
-class Cat extends Animal {
-  public function meaw(): string { return "Meaw!" }
+// In plain object, the usage is same:
+function printVal(val: { a: number } | { b: string }) {
+  if ("a" in val) { // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    console.log(val.a); // x is { a: number }
+  } else {
+    console.log(val.b); // x is { b: string }
 }
 
-class Dog extends Animal {
-  public function woof(): string { return "Woof!" }
-}
+// @@@ Narrowing with Array.isArray
 
-class Animal {
-  isCat(): this is Cat { // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    return this instanceof Cat;
-  }
-  isDog(): this is Dog { // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    return this instanceof Dog;
-  }
-}
-
-function makeSound(animal: Animal): string {
-	if (animal.isCat()) {
-		return animal.meaw();
-	} else if (animal.isDog()) {
-		return animal.woof();
-	} else {
-		return "Uuuuuuu!!!";
-	}
-}
-
-// @@@ Truthiness narrowing:
-// We can use any expression in conditionals, && s, || s, if statements, and Boolean negations ( ! ), and more.
-// As an example, if statements don't expect their condition to always have the type boolean:
-function getUsersOnlineMessage(numUsersOnline: number) {
-	if (numUsersOnline) {
-		return 'There are ${numUsersOnline} online now!';
-	}
-		return "Nobody's here. :(";
-	}
-}
-
-// Another example is to use a function like Array.isArray:
+// To check if an object is an Array, use Array.isArray:
 function welcomePeople(x: string[] | string) {
   if (Array.isArray(x)) {
     console.log("Hello, " + x.join(" and "));
@@ -132,7 +83,7 @@ function getFirstThree(x: number[] | string) {
   return x.slice(0, 3); // return type is inferred as number[] | string
 }
 
-//### Assertion Functions
+// @@@ Assertion Functions
 
 // Assertion functions in TypeScript are special functions used to assert certain conditions in your code.
 // When you use an assertion function, TypeScript narrows the type based on the asserted condition.
@@ -159,19 +110,19 @@ function printLength(value: any) {
 printLength("Hello"); // Output: 5
 printLength(123);     // Throws an error: Value is not a string
 
-//### Functions overloading as a type-guard:
+// @@@ Functions overloading as a type-guard:
 
 // Since JavaScript is a dynamic language, we can often call the same function with different types of arguments. Consider the following code:
-function add(x,y) {
+function add(x, y) {
 	return x + y;
 }
-let result1 = add(1,1); // 2
-let result2 = add("1","1"); // "11"
+let result1 = add(1, 1); // 2
+let result2 = add("1", "1"); // "11"
 // In TypeScript, you can add strong typing using an overload-like syntax.
 // The first two definitions just check the types of the parameters and, if they are good, call the function itself (with the body).
 function add(a: string, b: string) : string; // no body!
 function add(a: number, b: number) : number; // no body!
 function add(a: any, b: any): any {return a + b;} // the final signature (which includes the function body) MUST use the any type!
-let result1 = add(1,1); // 2
-let result2 = add("1","1"); // "11"
-let result3 = add(1,false); // error: Argument of type 'true' is not assignable to parameter of type 'number'
+let result1 = add(1, 1); // 2
+let result2 = add("1", "1"); // "11"
+let result3 = add(1, false); // error: Argument of type 'true' is not assignable to parameter of type 'number'
