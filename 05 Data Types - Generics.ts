fafdd2ea65adda466e0c489cf0412e97928dfbd4
Ursego <90226705+Ursego@users.noreply.github.com>
2025-05-17 23:18:24 -0400

    Add files via upload

--- 05 Data Types - Generics.ts
+++ 05 Data Types - Generics.ts
@@ -4,36 +4,42 @@
 // An array with generics can describe the values that the array contains.
 type StringArray = Array<string>;
 type NumberArray = Array<number>;
-type ObjectWithNameArray = Array<{ name: string }>;
+type NameArray = Array<{ name: string }>;
 
 // You can declare your own types that use generics:
-interface Backpack<Type> {
-  add: (obj: Type) => void;
-  get: () => Type;
+interface Backpack<T> {
+  add: (obj: T) => void;
+  get: () => T;
 }
 declare const backpack: Backpack<string>; // a shortcut to tell TypeScript there is a constant called `backpack`, and to not worry about where it came from
-const b = backpack.get(); // object is a string, because we declared it above as the variable part of Backpack
-backpack.add(23); // ERROR: Argument of type 'number' is not assignable to parameter of type 'string'.
+const b = backpack.get(); // b is a string, because get() of Backpack<string> returns string
+backpack.add(23); // ERROR: Argument of type 'number' is not assignable to parameter of type 'string'
 
 // Generics in TypeScript provide a way to create reusable components that can work with a variety of data types.
 // They enable developers to define functions, classes, interfaces, and methods that can operate on different types without sacrificing type safety.
 
-// Generic Function
+// @@@ Generic Function
+
 // A generic function can operate on different types while retaining the type information.
 
 function identity<T>(arg: T): T {
   return arg;
 }
-let num = identity<number>(42); // Using the generic function with a number
-console.log(num); // Outputs: 42
-let str = identity<string>("Hello");// Using the generic function with a string
-console.log(str); // Outputs: Hello
-let inferredNum = identity(123); // Type inference allows us to omit the type argument
-console.log(inferredNum); // Outputs: 123
-let inferredStr = identity("World");
-console.log(inferredStr); // Outputs: World
 
-// Generic Class
+let num = identity<number>(42); // using the generic function with a number
+console.log(num); // outputs: 42
+
+let str = identity<string>("Hello");// using the generic function with a string
+console.log(str); // outputs: Hello
+
+let inferredNum = identity(123); // type inference allows us to omit the type argument
+console.log(inferredNum); // outputs: 123
+
+let inferredStr = identity("World");
+console.log(inferredStr); // outputs: World
+
+// @@@ Generic Class
+
 // A generic class can operate on different types specified when the class is instantiated.
 
 class Box<T> {
@@ -47,18 +53,21 @@ class Box<T> {
     return this.content;
   }
 }
-let stringBox = new Box<string>("A string"); // Creating an instance of the generic class with a string
-console.log(stringBox.getContent()); // Outputs: A string
-let numberBox = new Box<number>(123); // Creating an instance of the generic class with a number
-console.log(numberBox.getContent()); // Outputs: 123
 
-// Generic Interface
+let stringBox = new Box<string>("A string"); // creating an instance of the generic class with a string
+console.log(stringBox.getContent()); // outputs: A string
+
+let numberBox = new Box<number>(123); // creating an instance of the generic class with a number
+console.log(numberBox.getContent()); // outputs: 123
+
+// @@@ Generic Interface
+
 // A generic interface can be used to define the shape of objects with various types.
 
 // A generic interface that defines a pair of values of different types:
-interface Pair<T, U> {
-  first: T;
-  second: U;
+interface Pair<F, S> {
+  first: F;
+  second: S;
 }
 
 let numberStringPair: Pair<number, string> = {
@@ -71,7 +80,8 @@ let stringBooleanPair: Pair<string, boolean> = {
   second: true
 };
 
-// Generic Constraints
+// @@@ Generic Constraints
+
 // Generic constraints allow you to restrict the types that can be used with generics.
 
 interface Lengthwise {
@@ -86,30 +96,28 @@ function logLength<T extends Lengthwise>(arg: T): T {
   return arg;
 }
 
-// This works because string has a length property
-logLength("Hello"); // Outputs: 5
+// This works because string has a length property:
+logLength("Hello"); // outputs: 5
 
-// This works because array has a length property
-logLength([1, 2, 3]); // Outputs: 3
+// This works because array has a length property:
+logLength([1, 2, 3]); // outputs: 3
 
-// This would cause an error because number does not have a length property
+// This would cause an error because number does not have a length property:
 // logLength(123); // Error: Argument of type 'number' is not assignable to parameter of type 'Lengthwise'.
 
+// @@@ Generic type parameter
 
-
-// Generic (generic type parameter):
 type FilterFuncType = {
-	<T>(array: T[], callbackFunc: (item: T) => boolean): T[] // TypeScript will INFER the type from the actually passed values
+	<T>(arr: T[], callbackFunc: (item: T) => boolean): T[] // TypeScript will INFER the type from the actually passed values
 }
-let filter: FilterFuncType = (array, callbackFunc) => {
-	let result = []
-	for (let i = 0; i < array.length; i++) {
-		let item = array[i]
+let filter: FilterFuncType = (arr, callbackFunc) => {
+	let resultArr = []
+	for(let item of arr) {
 		if (callbackFunc(item)) {
-			result.push(item)
+			resultArr.push(item)
 		}
 	}
-	return result
+	return resultArr
 }
 // TypeScript INFERS the type as number:
 filter([1, 2, 3], _ => _ > 1) // returns [2, 3]
@@ -123,36 +131,7 @@ let namesArray = [
 ]
 filter(namesArray, _ => _.firstName.startsWith('b')) // returns [{firstName: 'beth'}, {firstName: 'buba'}]
 
-// We declared <T> as part of the call signature (before the parentheses), and TypeScript will bind a concrete type to T when we call a function of type FilterFuncType. If we had instead limited the range of T to the type alias FilterFuncType, TypeScript would require us to explicitly bind the type when using FilterFuncType:
-// T is declared as part of the type FilterFuncType (not part of the concrete type signature), and TypeScript will bind T when you declare a function of type FilterFuncType:
-type FilterFuncType<T> = {
-	(array: T[], f: (item: T) => boolean): T[]
-}
-let filter: FilterFuncType = (array, f) => // ... // Error: Generic type 'Filter' requires 1 type argument.
-type OtherFilter = FilterFuncType // Error: Conditional type 'FilterFuncType' requires 1 type argument.
-let filter: FilterFuncType<number> = (array, f) => // ...
-type StringFilterFuncType = FilterFuncType<string>
-let stringFilter: StringFilterFuncType = (array, f) => // ...
-
-// The only valid place to declare a generic type in a type alias is immediately after the type alias name and before its assignment (=).
-// Let's define a type MyEvent that describes a DOM event like click or mousedown:
-type MyEvent<T> = {
-	target: T
-	type: string
-}
-type ButtonEvent = MyEvent<HTMLButtonElement>
-let myEvent: MyEvent<HTMLButtonElement | null> = {
-	target: document.querySelector('#myButton'),
-	type: 'click'
-}
-// You can also use a generic type alias in the function signature. When TypeScript binds the type to T, it will also bind it to MyEvent:
-function triggerEvent<T>(event: MyEvent<T>): void {
-	// ...
-}
-triggerEvent({ // T is Element | null
-	target: document.querySelector('#myButton'),
-	type: 'mouseover'
-})
+// @@@ Generic type parameter with a default value
 
 // Just as you give function parameters default values, you can give generic types presets:
 type MyEvent<T = HTMLElement> = {
@@ -164,14 +143,14 @@ type MyEvent<T extends HTMLElement = HTMLElement> = {
 	target: T
 	type: string
 }
-// Like optional parameters in a function, generic types with presets should come after generic types without presets:
-// Good:
-type MyEvent2<Type extends string, Target extends HTMLElement = HTMLElement> = {
+// Like optional parameters in a function, generic types with presets should come after generic types without default values:
+
+type MyEvent2<Type extends string, Target extends HTMLElement = HTMLElement> = { // good!
 	target: Target
 	type: Type
 }
-// Bad:
-type MyEvent3<Target extends HTMLElement = HTMLElement, Type extends string> = { // Error: Required type parameters cannot follow optional ones
+
+type MyEvent3<Target extends HTMLElement = HTMLElement, Type extends string> = { // Error: Required type parameters may not follow optional type parameters
 	target: Target
 	type: Type
 }
